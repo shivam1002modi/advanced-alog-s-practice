7dfb521e5c4c57935fecce498c3f482a59c62314
shivam modi <159185532+Shivam2005modi@users.noreply.github.com>
2025-07-02 04:40:50 +0000

    MORE SIMPLE CODE (UPDATED CODE )

--- randomized_find.cpp
+++ randomized_find.cpp
@@ -1,60 +1,69 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
 using namespace std;
 
-void swap(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;
+// Function to swap two elements
+void swap(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
 }
 
-int partition(int arr[], int left, int right) {
-    int pivot = arr[right];
+// Function to partition the array around a pivot
+int partition(int a[], int left, int right) {
+    int pivot = a[right];
     int i = left - 1;
 
-    for (int j = left; j <= right - 1; j++) {
-        if (arr[j] <= pivot) {
+    for (int j = left; j < right; j++) {
+        if (a[j] <= pivot) {
             i++;
-            swap(arr[i], arr[j]);
+            swap(a[i], a[j]);
         }
     }
 
-    swap(arr[i + 1], arr[right]);
-    return (i + 1);
+    swap(a[i + 1], a[right]);
+    return i + 1;
 }
 
-int randomizedFind(int arr[], int left, int right, int k) {
+// Randomized Find function to get kth smallest element
+int randomizedFind(int a[], int left, int right, int k) {
     if (left <= right) {
-        int pivotIndex = rand() % (right - left + 1) + left;
-        swap(arr[pivotIndex], arr[right]);
+        // Step 1: pick random number y from s
+        int randomIndex = rand() % (right - left + 1) + left;
+        swap(a[randomIndex], a[right]);
 
-        int q = partition(arr, left, right);
-        int count = q - left + 1;
+        // Step 2: partition around y
+        int pos = partition(a, left, right); // pos is index of y
+        int numOnLeft = pos - left; // how many elements < y
 
-        if (count == k) {
-            return arr[q];
-        } else if (k < count) {
-            return randomizedFind(arr, left, q - 1, k);
-        } else {
-            return randomizedFind(arr, q + 1, right, k - count);
+        // Step 3: check conditions
+        if (numOnLeft == k - 1) {
+            return a[pos]; // y is the kth smallest
+        }
+        else if (numOnLeft > k - 1) {
+            // find in left part
+            return randomizedFind(a, left, pos - 1, k);
+        }
+        else {
+            // find in right part
+            return randomizedFind(a, pos + 1, right, k - numOnLeft - 1);
         }
     }
 
-    return -1; // In case of invalid input
+    return -1; // base case if not found
 }
 
 int main() {
-    srand(time(0));
+    srand(time(0)); // for randomness
 
-    int arr[] = {2, 8, 3, 9, 7, 16, 4};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int a[] = {2, 8, 3, 9, 7, 16, 4};
+    int n = sizeof(a) / sizeof(a[0]);
     int k = 5;
 
-    int result = randomizedFind(arr, 0, n - 1, k);
+    int ans = randomizedFind(a, 0, n - 1, k);
 
-    cout << "The " << k << "th smallest element is: " << result << endl;
+    cout << "The " << k << "th smallest element is: " << ans << endl;
 
     return 0;
 }
